4d6710bf5647feaab3ae49228ed0d0b17b56f93f
Nguyễn Phú Quí <69145540+npqui1406@users.noreply.github.com>
2022-04-08 15:30:00 +0700

    Add files via upload

--- Data_Oracle/ThuHoi.sql
+++ Data_Oracle/ThuHoi.sql
@@ -1,5 +1,5 @@
 -- Thu hoi quyen cua user/role:
-create or replace procedure proc_revoke_user(ip_user in varchar2, ip_table in varchar2, ip_pos in varchar2) -- select, insert, delete
+create or replace procedure proc_revoke_user(ip_user in varchar2, ip_table in varchar2, ip_pos in varchar2) -- select, insert, delete, update
 is
 user_name varchar2(20):= ip_user;
 table_name varchar2(30):=ip_table;
@@ -17,24 +17,7 @@ begin
 commit;
 end proc_revoke_user;
 /
-create or replace procedure proc_revoke_user_update(ip_user in varchar2, ip_table in varchar2, ip_column in varchar2) -- update
-is
-user_name varchar2(20):= ip_user;
-table_name varchar2(30):=ip_table;
-column_name varchar2(30):=ip_column;
-exec_commander varchar(1000);
-begin
-    exec_commander:='ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE';
-    EXECUTE IMMEDIATE(exec_commander);
-    
-    exec_commander:='REVOKE UPDATE' || '(' || column_name ||') ON '|| table_name ||' FROM ' || user_name;
-    EXECUTE IMMEDIATE(exec_commander);
-
-    exec_commander:='ALTER SESSION SET "_ORACLE_SCRIPT"=FALSE';
-    EXECUTE IMMEDIATE(exec_commander);
-commit;
-end proc_revoke_user_update;
-/
+-- Thu hoi role tu user
 create or replace procedure proc_revoke_user_role(ip_user in varchar2, ip_role in varchar2)
 is
 user_name varchar2(20):= ip_user;
