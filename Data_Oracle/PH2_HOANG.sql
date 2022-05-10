16f89267d1969252e2d7c9e3aa357f8e215cbfe5
HuyHoangN <68884802+HuyHoangN@users.noreply.github.com>
2022-05-11 02:20:40 +0700

    Update PH2_HOANG.sql

--- Data_Oracle/PH2_HOANG.sql
+++ Data_Oracle/PH2_HOANG.sql
@@ -1,64 +1,55 @@
+PROCEDURE  xem có the cap quyen select cho nguoi dùng có column level
+CREATE OR REPLACE PROCEDURE proc_CreateViewUserSelectColumnLevel(
+    ip_username nvarchar2,
+    ip_column_name nvarchar2,
+    ip_table_name nvarchar2,
+    ip_granable nvarchar2
+    )
+IS
+    user_name nvarchar2(200):=ip_username;
+    column_name nvarchar2(200):=ip_column_name;
+    table_name nvarchar2(200):=ip_table_name;
+    exec_commander varchar(1000);
+    priv nvarchar2(200);
+    vw_name nvarchar2(200);
+BEGIN 
+    priv:='SELECT';
+    exec_commander:='ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE ';
+    EXECUTE IMMEDIATE (exec_commander);
+    
+    IF UPPER(ip_granable) = 'TRUE' THEN
 
+     exec_commander:='CREATE VIEW VW_USER_SELECT_COLUMN_LEVEL_'||ip_username||'_'||ip_column_name||'_'||ip_table_name
+    || ' AS '|| 'SELECT ' ||column_name|| ' FROM '|| table_name;
+    EXECUTE IMMEDIATE (exec_commander);
+    
+    
+        vw_name:='VW_USER_SELECT_COLUMN_LEVEL_'||ip_username||'_'||ip_column_name||'_'||ip_table_name;
+    exec_commander:='GRANT SELECT ON '||vw_name || ' TO ' ||ip_username|| ' WITH GRANT OPTION ';
+        EXECUTE IMMEDIATE (exec_commander);
+           
+    ELSE 
+     exec_commander:='CREATE VIEW VW_USER_SELECT_COLUMN_LEVEL_'||ip_username||'_'||ip_column_name||'_'||ip_table_name
+    || ' AS '|| 'SELECT ' ||column_name|| ' FROM '|| table_name;
+    EXECUTE IMMEDIATE (exec_commander);
+    
+      vw_name:='VW_USER_SELECT_COLUMN_LEVEL_'||ip_username||'_'||ip_column_name||'_'||ip_table_name;
+    exec_commander:='GRANT SELECT ON '||vw_name || ' TO ' ||ip_username;
+        EXECUTE IMMEDIATE (exec_commander);
+    
+    END IF;
+
+    
+   exec_commander:='ALTER SESSION SET "_ORACLE_SCRIPT" = FALSE ';
+    EXECUTE IMMEDIATE (exec_commander);
+END proc_CreateViewUserSelectColumnLevel;
+/
 CREATE ROLE ROLE_DOCTOR IDENTIFIED BY ROLE_DOCTOR;
-GRANT CREATE SESSION TO ROLE_DOCTOR;
-GRANT SELECT ON HSBA_DV TO ROLE_DOCTOR;
-GRANT SELECT ON CTDONTHUOC TO ROLE_DOCTOR;
-GRANT SELECT ON DICHVU TO ROLE_DOCTOR;
-GRANT SELECT ON CSYT TO ROLE_DOCTOR;
-GRANT INSERT(ketLuanCuaBacSi), UPDATE(ketLuanCuaBacSi)ON BENHNHAN TO ROLE_DOCTOR;
-GRANT INSERT (maKB,maDV,ngayGio), UPDATE (maKB,maDV,ngayGio) ON DICHVU TO ROLE_DOCTOR;
-GRANT INSERT, UPDATE (ketLuanCuaBacSi) ON BENHNHAN TO ROLE_DOCTOR;
-GRANT INSERT, UPDATE (maKB,maDV,ngayGio) ON DICHVU TO ROLE_DOCTOR;
-
-CREATE OR REPLACE FUNCTION FUNC_VPD_POLICY_DOCTOR_HOSOBENHNHAN(
-    v_schema IN VARCHAR2,
-    v_object IN VARCHAR2
-)
-RETURN VARCHAR2 
-AS
-    predicate VARCHAR2(200);
-    cur_user VARCHAR2(200);
-BEGIN
-
-    cur_user:= SYS_CONTEXT('USERENV','SESSION_USER');
-    IF (INSTR(cur_user,'BACSI')<>0) THEN
-      predicate:= 'TENBACSI = ''' ||cur_user||'''';
-    ELSE
-     predicate:= '';
-    END IF;
-    RETURN predicate;
-END FUNC_VPD_POLICY_DOCTOR_HOSOBENHNHAN;
-/
-
-
-alter session set "_ORACLE_SCRIPT"=true; 
-CREATE ROLE role_dep_nghiencuu IDENTIFIED BY role_dep_nghiencuu;
---Grant policy
-GRANT SELECT ON BENHNHAN TO role_dep_nghiencuu;
-GRANT SELECT ON csyt TO role_dep_nghiencuu;
-GRANT SELECT ON BENHNHAN TO role_dep_nghiencuu;
-GRANT SELECT ON DICHVU TO role_dep_nghiencuu;
---Grant role to user
-GRANT role_dep_nghiencuu TO user_nghiencuu_01;
-GRANT role_dep_nghiencuu TO user_nghiencuu_02;
-GRANT role_dep_nghiencuu TO user_nghiencuu_03;
-
-CREATE OR REPLACE FUNCTION FUNC_VPD_POLICY_NGHIENCUU(
-    v_schema IN VARCHAR2,
-    v_object IN VARCHAR2
-)
-RETURN VARCHAR2 
-AS
-    predicate VARCHAR2(200);
-    cur_user VARCHAR2(200);
-BEGIN
-
-    cur_user:= SYS_CONTEXT('USERENV','SESSION_USER');
-    IF (INSTR(cur_user,'BACSI')<>0) THEN
-      predicate:= 'TENBACSI = ''' ||cur_user||'''';
-    ELSE
-     predicate:= '';
-    END IF;
-    RETURN predicate;
-END FUNC_VPD_POLICY_DOCTOR_NGHIENCUU;
-/
+grant select on NHANVIEN to ROLE_DOCTOR;
+grant update on NHANVIEN to ROLE_DOCTOR;
+CREATE ROLE role_nghiencuu IDENTIFIED BY role_nghiencuu;
+grant select on NHANVIEN to role_nghiencuu;
+grant update on NHANVIEN to role_nghiencuu;
+CREATE ROLE role_benhnhan IDENTIFIED BY role_benhnhan;
+grant select on benhnhan to role_nghiencuu;
+grant update on benhnhan to role_nghiencuu;
